350396c0b3489f9f7576d42a10543b9b89600f8d
EoD <EoD@xmw.de>
2014-06-22 10:39:36 +0200

    some code cleanup

--- crackpin.c
+++ crackpin.c
@@ -48,6 +48,7 @@
 #define	KEY_LEN_BYTES	16
 #define IV_LEN_BYTES	16
 #define HASH_COUNT	2000
+#define PIN_SIZE	4
 
 /* crypto footer structure (from cryptfs.h of Android sources) */
 struct crypto_ftr {
@@ -63,6 +64,9 @@ struct crypto_ftr {
 	uint8_t crypto_type_name[64];
 };
 
+int decrypt_decode_key(uint8_t *kekiv, uint8_t *salt, unsigned char *pin, 
+			const md_info_t *info_sha1, md_context_t sha1_ctx);
+
 /* 
  * crack 4-digit PINs directly on the phone
  * (for more digits we suggest to crack the PIN on your x86 machine)
@@ -71,19 +75,29 @@ int main()
 {
 	struct crypto_ftr *ftr;
 	uint8_t *header=NULL, *footer=NULL, *salt, *encdek, *kek;
-	int i, header_fd=-1, footer_fd=-1;
+	int i, header_fd=-1, footer_fd=-1, error;
 	uint8_t kekiv[KEY_LEN_BYTES+IV_LEN_BYTES], iv[IV_LEN_BYTES];
 	uint8_t decdek[KEY_LEN_BYTES], decheader[HEADER_SIZE];
 	md_context_t sha1_ctx; aes_context aes_ctx;
 	const md_info_t *info_sha1;
-	unsigned char pin[5] = {0,0,0,0,0};
+	unsigned char pin[PIN_SIZE+1] = {0,0,0,0,0};
 
 	/* cleanup: close header and footer mappings */
-	void quit(char *msg, int err) {
+	void quit(int err) {
+		char* msg;
 		if (header) munmap(header, HEADER_SIZE);
 		if (footer) munmap(footer, HEADER_SIZE);
 		if (header_fd != -1) close(header_fd);
 		if (footer_fd != -1) close(footer_fd);
+		switch (err) {
+			case 0: msg = ""; break;
+			case 3: msg = "SHA1 context error.\n"; break;
+			case 4: msg = "PBKDF2 error.\n"; break;
+			case 5: msg = "AES error with KEK.\n"; break;
+			case 6: msg = "AES/CBC error with DEK.\n"; break;
+			case 7: msg = "\nSorry, no 4-digit PIN matches.\n\n"; break;
+			default: msg = "Unknown Error.\n"; break;
+		}
 		printf("%s",msg);
 		exit(err);
 	}
@@ -136,13 +150,10 @@ int main()
 			printf("...trying %s\n",pin);
 		}
 
-		/* make key encryption key and iv from PIN */
-		if (!(info_sha1 = md_info_from_type(POLARSSL_MD_SHA1)) || md_init_ctx(&sha1_ctx, info_sha1)) {
-			quit("SHA1 context error.\n",3);
-		}
-		if (pbkdf2_hmac(&sha1_ctx, pin, 4, salt, SALT_SIZE, HASH_COUNT, KEY_LEN_BYTES+IV_LEN_BYTES, kekiv)) {
-			quit("PBKDF2 error.\n",4);
-		}
+		error = decrypt_decode_key(kekiv, salt, pin, info_sha1, sha1_ctx);
+		if(error)
+			quit(error);
+
 		kek = kekiv;
 		for (i=0; i<IV_LEN_BYTES; i++) {
 			iv[i] = kekiv[KEY_LEN_BYTES+i];
@@ -151,14 +162,14 @@ int main()
 		/* decrypt data encryption key with the key encryption key */
 		aes_setkey_dec(&aes_ctx, kek, KEY_LEN_BYTES*8);
 		if (aes_crypt_cbc(&aes_ctx, AES_DECRYPT, KEY_LEN_BYTES, iv, encdek, decdek)) {
-			quit("AES error with KEK.\n",5);
+			quit(5);   // AES error with KEK
 		}
 
 		/* decrypt actual data with the data encryption key */
 		aes_setkey_dec(&aes_ctx, decdek, KEY_LEN_BYTES*8);
 		memset(iv,0,IV_LEN_BYTES);
 		if (aes_crypt_cbc(&aes_ctx, AES_DECRYPT, HEADER_SIZE, iv, header, decheader)) {
-			quit("AES/CBC error with DEK.\n",6);
+			quit(6);   // AES/CBC error with DEK
 		}
 
 		/* check correctness of decrypted header */
@@ -171,7 +182,7 @@ int main()
 	}
 
 	/* no success :( */
-	quit("\nSorry, no 4-digit PIN matches.\n\n",7);
+	quit(7);
 
 	/* success: print found PIN/key */
 	success:
@@ -185,9 +196,22 @@ int main()
 	for (i=0; i<KEY_LEN_BYTES; i++)
 		printf("%02x",decdek[i]);
 	printf("\n\n            PIN: %s\n\n",pin);
-	quit("",0);
+	quit(0);
 
 	return 0; /* suppress stupid compiler warnings */
 }
 
 int raise(int x) { return x; } /* strange NDK cross compiling bug fix */
+
+int decrypt_decode_key(uint8_t *kekiv, uint8_t *salt, unsigned char *pin,
+			const md_info_t *info_sha1, md_context_t sha1_ctx) {
+	/* make key encryption key and iv from PIN */
+	if (!(info_sha1 = md_info_from_type(POLARSSL_MD_SHA1)) || md_init_ctx(&sha1_ctx, info_sha1)) {
+		return 3;   // SHA1 context error
+	}
+	if (pbkdf2_hmac(&sha1_ctx, pin, PIN_SIZE, salt, SALT_SIZE, HASH_COUNT, KEY_LEN_BYTES+IV_LEN_BYTES, kekiv)) {
+		return 4;   // PBKDF2 error
+	}
+
+	return 0;
+}
